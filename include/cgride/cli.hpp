67a4286210593f9476e63ea05a30c266119e53f4
Gaspard Kirira <gaspardkirira9@gmail.com>
2026-07-16 15:01:48 +0300

    Add strict cross-platform CI

---
+++ include/cgride/cli.hpp
@@ -0,0 +1,29 @@
+/**
+ *
+ *  @file cli.hpp
+ *  @author Gaspard Kirira
+ *
+ *  Copyright 2026, Gaspard Kirira.
+ *  All rights reserved.
+ *  https://github.com/cgride/cli
+ *
+ *  Use of this source code is governed by an MIT license
+ *  that can be found in the LICENSE file.
+ *
+ *  Cgride
+ *
+ */
+#ifndef CGRIDE_CLI_HPP
+#define CGRIDE_CLI_HPP
+
+#include <cgride/cli/application.hpp>
+#include <cgride/cli/build_command.hpp>
+#include <cgride/cli/cli_options.hpp>
+#include <cgride/cli/command_context.hpp>
+#include <cgride/cli/command_line.hpp>
+#include <cgride/cli/exit_code.hpp>
+#include <cgride/cli/run_command.hpp>
+#include <cgride/cli/terminal.hpp>
+#include <cgride/cli/version.hpp>
+
+#endif // CGRIDE_CLI_HPP
